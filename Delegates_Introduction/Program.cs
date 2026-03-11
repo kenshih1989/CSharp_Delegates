e14ca0ff9ee0104180470cb08ad178cff77a3bff
Gan Leng Keong <kenshih_keong@hotmail.com>
2026-03-11 22:23:49 +0800

    Add project files.

---
+++ Delegates_Introduction/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Delegates_Introduction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
