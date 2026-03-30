6f93702ecdfd9072aa45ab7ec69d1544a7730a02
Gan Leng Keong <kenshih_keong@hotmail.com>
2026-03-30 14:36:39 +0800

    Add project files.

---
+++ Delegates_BaiscExample/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Delegates_BaiscExample
+{
+    internal class Program
+    {
+        // Define the delegate
+        delegate void LogDelegate(string message);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
