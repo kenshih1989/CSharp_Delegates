f2cacaa24d84762ecae74a126ccc7b3bb751fd71
Gan Leng Keong <kenshih_keong@hotmail.com>
2026-03-30 16:09:55 +0800

    Commit the ClubMembershipApplication

---
+++ Delegates_ClubMembershipApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using ClubMembershipApplication.Views;
+
+namespace ClubMembershipApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IView mainView = Factory.CreateMainView();
+            mainView.RunView();
+
+            Console.ReadKey();
+        }
+    }
+}
